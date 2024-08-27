8803c49d46c882b358b37db87bb8875cd14bdcdb
SALAMSASA <kwkwkyz61@gmail.com>
2024-08-27 22:16:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002051286023))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 7004732448))
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/A1DIIU")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/A1DIIU")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ll0llld")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", "True"))
